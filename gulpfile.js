0a27f4c5b38a65c1c3d0c3a575cc92145f1b8875
Matt B <mattia.bonacini@gmail.com>
2022-06-22 19:17:25 +0300

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,33 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass')(require('sass'));
+const browserSync = require('browser-sync').create();
+
+function styles(done) {
+  // Specify path of source SASS / SCSS file
+  return gulp.src('sass/**/*.scss')
+  // Compile it, and catch errors
+  .pipe(sass().on('error',sass.logError))
+  // Save the CSS output file in this path
+  .pipe(gulp.dest('.'))
+  // Auto-inject into browsers
+  .pipe(browserSync.stream());
+
+  done();
+}
+
+function watch(done) {
+    browserSync.init({
+      proxy: "http://testyt.local",
+    });
+
+    // Watch for changes in the source SASS / SCSS file
+    gulp.watch('sass/**/*.scss', styles);
+    
+    gulp.watch('./**/*.css').on('change',browserSync.reload);
+    gulp.watch('js/*.js').on('change',browserSync.reload);
+    gulp.watch('./**/*.php').on('change',browserSync.reload);
+
+    done();
+}
+
+exports.watch = watch;
